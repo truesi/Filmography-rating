7ae4b119b9df0dcd941528c338a8cce0e5b2a09b
askar@eca.kz <askar@eca.kz>
2019-10-03 03:52:08 +0600

    added communication with eureka server

--- src/main/java/com/retake/filmographyrating/RatingController.java
+++ src/main/java/com/retake/filmographyrating/RatingController.java
@@ -1,17 +1,31 @@
 package com.retake.filmographyrating;
 
+import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/rating")
 public class RatingController {
 
-    @GetMapping("{/movieID}")
-    public Rating getRatingByMovieID(
-            @PathVariable("movieID")String movieID){
+    @GetMapping("/{movieID}")
+    public Rating getRatingByMovieID(@PathVariable("movieID") String movieID){
         return new Rating(movieID, 1);
+    }
+
+    @GetMapping("users/{userID}")
+    public UserRating getRating(@PathVariable("userID") String userId){
+        List<Rating> ratings = Arrays.asList(
+                new Rating("Title",3),
+                new Rating("Title2",112)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+        return userRating;
     }
 }
---
+++ src/main/java/com/retake/filmographyrating/UserRating.java
@@ -0,0 +1,11 @@
+package com.retake.filmographyrating;
+
+import lombok.Data;
+
+import java.util.List;
+
+public @Data class UserRating {
+
+    private List<Rating> userRating;
+
+}
